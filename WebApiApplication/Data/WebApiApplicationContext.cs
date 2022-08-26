e7cf99f60d8a21f66406510bc4d7a556e2a77900
unknown <cogdotnet1686@iiht.tech>
2022-08-26 15:57:51 +0530

    Initial commit

---
+++ WebApiApplication/Data/WebApiApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiApplication.Model;
+
+namespace WebApiApplication.Data
+{
+    public class WebApiApplicationContext : DbContext
+    {
+        public WebApiApplicationContext (DbContextOptions<WebApiApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiApplication.Model.Order> Order { get; set; } = default!;
+    }
+}
---
+++ WebApiApplication/Model/Order.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiApplication.Model
+{
+    public class Order
+    {
+        [Key]
+        public int OderId { get; set; }
+        public string OrderItem { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int quantity { get; set; }
+        public decimal price { get; set; }
+        public string OredrStatus { get; set; }
+    }
+}
