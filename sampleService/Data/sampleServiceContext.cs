a3f466d4519ff96f492f597f287d0f63d9737535
unknown <cogdotnet1686@iiht.tech>
2022-08-30 20:12:16 +0530

    '30aug2022'

---
+++ sampleService/Data/sampleServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using sampleService.Model;
+
+namespace sampleService.Data
+{
+    public class sampleServiceContext : DbContext
+    {
+        public sampleServiceContext (DbContextOptions<sampleServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<sampleService.Model.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ sampleService/Model/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sampleService.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+}
